c6bd2f73fa5505d7ba01b15a94a55c5d81f414c3
maijinpei <maijinpei@flyfungame.com>
2020-12-24 17:39:40 +0800

    v1.1.0开发：提示版本号至v1.1.0

--- library_core/src/main/java/cn/flyfun/gamesdk/Version.kt
+++ library_core/src/main/java/cn/flyfun/gamesdk/Version.kt
@@ -7,8 +7,8 @@ package cn.flyfun.gamesdk
 @kotlin.annotation.Retention(AnnotationRetention.SOURCE)
 annotation class Version {
     companion object {
-        const val CORE_VERSION_NAME = "1.0.3"
-        const val CORE_VERSION_CODE = 103
+        const val CORE_VERSION_NAME = "1.1.0"
+        const val CORE_VERSION_CODE = 110
         const val SERVER_VERSION_NAME = "1.0.0"
     }
 }
